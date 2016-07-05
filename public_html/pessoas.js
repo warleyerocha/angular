7e4ed926e4b76fc4fe2178eb9d19b8fd1994718a
1674170 <1674170@GR002856.cefetes.br>
2016-07-05 09:23:47 -0300

    AA

---
+++ public_html/pessoas.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+angular
+    .module('pessoas', [])
+    .controller('CtrlPessoas', function($scope) {
+               $scope.pessoas = [
+                   {nome: "maria",cidade: "guarapari"},
+                   {nome: "joao",cidade: "vitoria"},
+                   {nome: "jose",cidade: "sete lagoas"},
+                   {nome: "nilce",cidade: "prudente"},
+                   {nome:"warley",cidade: "bh"}
+               ]
+   
+});
